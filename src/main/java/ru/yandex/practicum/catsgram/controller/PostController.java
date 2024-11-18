30ff03bfac9647211086e5ea939eef4c5a1ea581
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-11-19 02:51:18 +0300

    refactor: добавить в PostController зависимость PostService

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,30 +1,31 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 public class PostController {
-    //    private final static Logger log = LoggerFactory.getLogger(PostController.class);
-    private final List<Post> posts = new ArrayList<>();
+    private final PostService postService;
 
     @GetMapping("/posts")
     public List<Post> findAll() {
-        log.info("Получаем все посты в количестве: {}", posts.size());
-        return posts;
+        log.debug("Получаем все посты в количестве (controller): {}", postService.findAll().size());
+        return postService.findAll();
     }
 
     @PostMapping("/post")
     public void create(@RequestBody Post post) {
-        posts.add(post);
-        log.debug("Создаем пост: {}", post);
+        postService.findAll().add(post);
+        log.debug("Создаем пост (controller): {}", post);
     }
 }
